a2ecc9a307707e8d9fb94ff838d7e789ab543699
yoloha <iseanwang@gmail.com>
2016-12-29 20:23:08 +0800

    parse_prob_to_sumit_format

--- final/outbrain/parse_submit.py
+++ final/outbrain/parse_submit.py
@@ -2,10 +2,7 @@ import pandas as pd
 import numpy as np
 
 dtypes = {'clicked': np.float32}
-
 df = pd.read_csv("proba.csv", dtype=dtypes)
-print df
-
 df.sort_values(['display_id','clicked'], inplace=True, ascending=False)
 subm = df.groupby('display_id').ad_id.apply(lambda x: " ".join(map(str,x))).reset_index()
 subm.to_csv("subm.csv", index=False)
