08a6bf4cfd69af9d64d9cf7b6e2570b05eac46ad
yoloha <iseanwang@gmail.com>
2017-01-20 09:59:17 +0800

    remove_remote

--- final/outbrain/parse_events_doc.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-output = open('events_document.csv', 'wb')
-with open('events.csv') as f:
-	for line in f:
-		ls = line.split(',')
-		ls_sel = [ls[0], ls[2]]
-		output.write(",".join(map(str,ls_sel)))
-		output.write("\n")
